357c2794f1f544809074660f206179d335cd0773
Weichao Qiu <qiuwch@gmail.com>
2016-07-06 21:22:11 -0400

    Update eval code.

--- tools/eval_az_el.py
+++ tools/eval_az_el.py
@@ -1,9 +1,9 @@
 import _init_paths
 from fast_rcnn.test import test_net
-from fast_rcnn.config import cfg, cfg_from_file, cfg_from_list
+from fast_rcnn.config import cfg, cfg_from_file, cfg_from_list, get_output_dir
 from datasets.factory import get_imdb
 import caffe
-import time, os, sys, pprint
+import time, os, sys, pprint, cPickle
 
 cfg_file = './experiments/cfgs/faster_rcnn_alt_opt.yml'
 cfg_from_file(cfg_file)
@@ -36,12 +36,35 @@ if not cfg.TEST.HAS_RPN:
     imdb.set_proposal_method(cfg.TEST.PROPOSAL_METHOD)
 
 imdb_names = []
-for el in range(0, 61, 30):
-    for az in range(90, 271, 45):
+el_range = range(0, 61, 30)
+az_range = range(90, 271, 45)
+for el in el_range:
+    for az in az_range:
         imdb_names.append('unrealcv_%d_%d' % (el, az))
 
-for imdb_name in imdb_names:
-    print imdb_name
-    imdb = get_imdb(imdb_name)
-    imdb.competition_mode(comp_mode)
-    test_net(net, imdb, max_per_image=max_per_image, vis=vis)
+# for imdb_name in imdb_names:
+#     print imdb_name
+#     imdb = get_imdb(imdb_name)
+#     imdb.competition_mode(comp_mode)
+#     test_net(net, imdb, max_per_image=max_per_image, vis=vis)
+#
+
+cls = 'sofa'
+
+table = ''
+header = '%9s' % 'el/az' + ''.join(['%9.3f' % az for az in az_range])
+table += header + '\n'
+
+for el in el_range:
+    row = '%9d' % el
+    for az in az_range:
+        imdb_name = 'unrealcv_%d_%d' % (el, az)
+        imdb = get_imdb(imdb_name)
+        imdb.competition_mode(comp_mode)
+        output_dir = get_output_dir(imdb, net)
+        with open(os.path.join(output_dir, cls + '_pr.pkl')) as f:
+            result = cPickle.load(f)
+        row += '%9.3f' % result['ap']
+    table += row + '\n'
+
+print table
